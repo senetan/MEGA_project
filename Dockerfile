83e7793760ec53ba1350cef199feaf444acf6e7d
Anthony Clain <anthony5.clain@hotmail.fr>
2025-03-20 10:48:11 +0100

    clean and update API

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
-FROM python:3.10-slim
+FROM python:3.10.6
+
+COPY . /app
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
-COPY MEGA_project_folder MEGA_project_folder
-COPY models models
+COPY MEGA_project_folder /root/code/senetan/MEGA_project_folder
+COPY models /root/code/senetan/MEGA_project/models
+
+ENV MODEL_PATH=/root/code/senetan/MEGA_project/models/MEGA_model.h5
+ENV PIPELINE_PATH=/root/code/senetan/MEGA_project/models/MEGA_model.pkl
 
 CMD uvicorn MEGA_project_folder.api_file:app --host 0.0.0.0
--- setup.py
+++ setup.py
@@ -4,21 +4,13 @@ with open('requirements.txt') as f:
     content = f.readlines()
 
 requirements = [x.strip() for x in content if "git+" not in x]
-extra_files = [
-    'Dockerfile',
-    'Makefile',
-    'README.md',
-    'requirements.txt',
-    'models/MEGA_model.h5',
-    'models/MEGA_model.pkl',
-    'notebook/mvp_draft.ipynb',
-    'raw_data/df_de_merged.csv'
-]
+
 setup(
     name='MEGA_project_folder',
     version="0.0.1",
     install_requires=requirements,
     packages=find_packages(),
+    include_package_data=True,
     entry_points={
         'console_scripts': [
             'mega_api=MEGA_project_folder.api_file:main',
